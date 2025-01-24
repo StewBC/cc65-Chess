67e0f5c51234f2c318559de2bc186a923ad8b970
Stefan Wessels <swessels@email.com>
2025-01-23 16:07:18 -0800

    Fix path separator on Windows

--- Makefile-dsk.mk
+++ Makefile-dsk.mk
@@ -17,5 +17,5 @@ dsk: $(DSK)
 
 $(DSK): cc65-Chess.apple2
 	$(call CP, apple2/template.dsk $@)
-	java -jar $(AC) -p  $@ chess.system sys < $(shell cl65 --print-target-path)/apple2/util/loader.system
+	java -jar $(AC) -p  $@ chess.system sys < $(subst \,/,$(shell cl65 --print-target-path)/apple2/util/loader.system)
 	java -jar $(AC) -as $@ chess        bin < cc65-Chess.apple2
